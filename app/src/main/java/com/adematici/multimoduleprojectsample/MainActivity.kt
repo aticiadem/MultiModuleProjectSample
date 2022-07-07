d206f24a7f460dff332458f5796abfda53dc44b4
Adem Atici <adematicis41@gmail.com>
2022-07-07 18:49:05 +0300

    Initial commit

---
+++ app/src/main/java/com/adematici/multimoduleprojectsample/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.adematici.multimoduleprojectsample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.adematici.multimoduleprojectsample.ui.theme.MultiModuleProjectSampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MultiModuleProjectSampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MultiModuleProjectSampleTheme {
+        Greeting("Android")
+    }
+}
