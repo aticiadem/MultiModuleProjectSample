73a25a846675216c8a05b971568c51598a21f1f5
Adem Atici <adematicis41@gmail.com>
2022-07-21 16:12:05 +0300

    Add the domain module

---
+++ domain/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies { }
