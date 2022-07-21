b6ee7d34efd426a425cc672dbb311e4b81e4ba23
Adem Atici <adematicis41@gmail.com>
2022-07-21 15:23:02 +0300

    Add the buildSrc module

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,43 @@
+object Jvm {
+    const val target = "1.8"
+    const val inject = "1"
+}
+
+object Sdk {
+    const val min = 23
+    const val target = 32
+    const val compile = 32
+}
+
+object Kotlin {
+    const val compiler = "1.6.21"
+    const val compilerExtension = "1.2.0-rc02"
+    const val coroutines = "1.6.2"
+}
+
+object Compose {
+    const val composeVersion = "1.3.0-alpha01"
+    const val composeAlpha = "1.3.0-alpha01"
+}
+
+object BuildTools {
+    const val gradle = "7.2.1"
+}
+
+object Google {
+    const val hilt = "2.38.1"
+    const val material = "1.6.1"
+}
+
+object AndroidX {
+    const val core = "1.8.0"
+    const val appcompat = "1.4.2"
+    const val activityCompose = "1.5.0"
+    const val lifecycle = "2.5.0"
+}
+
+object Testing {
+    const val junitExt = "1.1.3"
+    const val espresso = "3.4.0"
+    const val junit = "4.13.2"
+}
